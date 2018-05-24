0135c9718f4ba7c577036f430c9ff0f01e5f9d78
BradHuang <adghbc01@tamu.edu>
2018-05-24 19:33:53 +0000

    display data

--- Projects/MovieAPI/app.js
+++ Projects/MovieAPI/app.js
@@ -7,12 +7,15 @@ app.set("view engine", "ejs");
 // https://web-dev-happitt.c9users.io/results
 app.get("/results", function(req, res) {
     // res.send("It works!");
-    request("http://www.omdbapi.com/?s=harry&apikey=thewdb", function(error, response, body) {
+    request("http://www.omdbapi.com/?s=x-men&apikey=thewdb", function(error, response, body) {
         if (!error && response.statusCode == 200) {
             // res.send(body); 
             // body is a String!
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            // res.send(results["Search"][0]["Title"]);
+            // res.render("results.ejs");
+            // res.render("results"); // results.ejs
+            res.render("results", {data: data}); // [object Object] 
         }
     });
 });
